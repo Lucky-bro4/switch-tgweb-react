a0ea07ceccc6d04b8fd0d567298ef8d21cac7f45
Lucky-bro4 <shuiskov2017@gmail.com>
2024-06-26 10:54:46 +0300

    show_alert

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,36 +5,36 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые', img: '/Images/Одежда/синие_джинсы.jpeg'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая', img: '/Images/Одежда/куртка.jpeg'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые', img: '/Images/Одежда/синие_джинсы.jpeg', img: ''},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая', img: '/Images/Одежда/куртка.jpeg', img: ''},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые', img: ''},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые', img: ''},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые', img: ''},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '9', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '10', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '11', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '12', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '13', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '14', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '15', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '16', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '17', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '18', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '19', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '20', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '21', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '22', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '23', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '24', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '25', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '26', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '27', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '28', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '29', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', img: ''},
+    {id: '30', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые', img: ''},
 ]
 
 const getTotalPrice = (items = []) => {
@@ -90,7 +90,7 @@ const ProductList = () => {
             //         4 максимум
             //     </Button>
             // </div>
-            tg.showAlert('4 максимум')
+            showAlert()
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
