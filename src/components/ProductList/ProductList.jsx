03745d0488c4235930ab142164271f86971c1d84
Lucky-bro4 <shuiskov2017@gmail.com>
2024-06-26 20:40:50 +0300

    fetch

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -53,7 +53,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId, 
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('https://switchlarge-lucky-bro4.amvera.io/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
