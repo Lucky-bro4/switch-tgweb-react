c8fd6920d44546fd6668922511de7f7bd51e2085
Lucky-bro4 <shuiskov2017@gmail.com>
2024-07-01 22:24:24 +0300

    ok

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -5,7 +5,8 @@ import Button from '../Button/Button';
 
 const AdminPage = () => {
     const [id, setId] = useState(String(products.length + 1));
-    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
     const [photo, setPhoto] = useState('/Images/Одежда/');
@@ -14,8 +15,12 @@ const AdminPage = () => {
         setId(e.target.value);
     };
 
-    const onChangeTitle = (e) => {
-        setTitle(e.target.value);
+    const onChangeCategory = (e) => {
+        setCategory(e.target.value);
+    };
+
+    const onChangeName = (e) => {
+        setName(e.target.value);
     };
 
     const onChangeDescription = (e) => {
@@ -47,11 +52,12 @@ const AdminPage = () => {
     };
 
     const sendData = async (e) => {
-        e.preventDefault(); // Отменить стандартное поведение формы
+        e.preventDefault();
 
         const newProduct = {
             id: id,
-            title: title,
+            category: category,
+            name: name,
             description: description,
             price: price,
             photo: photo
@@ -79,11 +85,18 @@ const AdminPage = () => {
                     onChange={onChangeId}
                 />
                 <input
+                    className={'category'}
+                    type="text"
+                    placeholder={'Category'}
+                    value={category}
+                    onChange={onChangeCategory}
+                />
+                <input
                     className={'input'}
                     type="text"
-                    placeholder={'Title'}
-                    value={title}
-                    onChange={onChangeTitle}
+                    placeholder={'Name'}
+                    value={name}
+                    onChange={onChangeName}
                 />
                 <input
                     className={'input'}
@@ -111,8 +124,8 @@ const AdminPage = () => {
                 </Button>
             </form>
             <div className='example'>
-                <img className={'example_img'} src={photo} alt={title} />
-                <div className={'example_title'}><b>{title}</b></div>
+                <img className={'example_img'} src={photo} alt={name} />
+                <div className={'example_name'}><b>{category + ' ' + name}</b></div>
                 <div className={'price'}>
                     <div className={'example_description'}>{description}</div>
                     <span>Стоимость: <b>{price}</b></span>
