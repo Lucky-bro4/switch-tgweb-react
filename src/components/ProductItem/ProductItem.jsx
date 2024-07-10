b6c3c64cfeacda23b856a63434b448876a11763d
Lucky-bro4 <shuiskov2017@gmail.com>
2024-07-11 01:09:28 +0300

    ok

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -36,7 +36,7 @@ const ProductItem = ({product, className, onAdd}) => {
             <div className={'description'}>{product.description}</div>
             <div className={'description'}>Размер: {product.size}</div>
             <Button className={status} onClick={onAddHandler}>
-                {content} / {product.rentPrice} р/д
+                {content} за {product.rentPrice} Р
             </Button>
             <div id='remove'>
                 {changeButton}
