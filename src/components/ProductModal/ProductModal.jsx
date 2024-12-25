13dbc00861bd97ed89dad11b43b7e4a27e389304
Lucky-bro4 <shuiskov2017@gmail.com>
2024-12-25 12:39:20 +0300

    ok

--- src/components/ProductModal/ProductModal.jsx
+++ src/components/ProductModal/ProductModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProductModal.css';
 import Button from "../Button/Button";
 import { Swiper, SwiperSlide } from 'swiper/react';
