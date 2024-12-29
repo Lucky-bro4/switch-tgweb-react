04ca7d5f32a1bb38e0ac907687a51973473902e8
Lucky-bro4 <shuiskov2017@gmail.com>
2024-12-29 13:21:34 +0300

    active-filters

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,13 +24,12 @@ const Header = () => {
 
     return (
         <div className='header'>
-            <div className="mainLogo">
-                <img
-                    src="/Images/mainLogo_withoutRental&Back.png"
-                    width={150}
-                    alt="Swich"
-                />
-            </div>
+            <img
+                src="/Images/mainLogo_withoutRental&Back.png"
+                width={150}
+                alt="Swich"
+                className="mainLogo"
+            />
         </div>
     );
 };
