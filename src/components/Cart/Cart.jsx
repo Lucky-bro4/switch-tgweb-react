4b59fc8a8c1e2495e2bbf21ecfda4d2c03c8e949
Lucky-bro4 <shuiskov2017@gmail.com>
2024-12-30 16:20:59 +0300

    ok

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, {useCallback, useEffect, useState} from "react";
 import { useNavigate } from 'react-router-dom';
 import { useTelegram } from "../../hooks/useTelegram";
+import ProductModal from "../ProductModal/ProductModal";
 import "./Cart.css";
 
 
--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useTelegram } from "../../hooks/useTelegram";
+import ProductModal from "../ProductModal/ProductModal";
 import "./Favorites.css";
 
 
