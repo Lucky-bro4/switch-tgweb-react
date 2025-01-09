c51e202a3d9c9e95bbc8fd7499355418b24110d4
Lucky-bro4 <shuiskov2017@gmail.com>
2025-01-09 13:18:02 +0300

    Update category selection options in AdminPage component for improved user experience

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -519,8 +519,11 @@ const AdminPage = () => {
                             onChange={onChangeCategory}
                             className="input"
                         >
-                            <option value="">Категория</option>
-                            <option value="Худи">Худи</option>
+                            <option value="">Выбери категорию</option>
+                            {Object.keys(categoryFields).map((key) => (
+                                <option key={key} value={key}>{key}</option>
+                            ))}
+                            {/* <option value="Худи">Худи</option>
                             <option value="Свитшот">Свитшот</option>
                             <option value="Футболка">Футболка</option>
                             <option value="Кофта">Кофта</option>
@@ -532,7 +535,7 @@ const AdminPage = () => {
                             <option value="Штаны">Штаны</option>
                             <option value="Джинсы">Джинсы</option>
                             <option value="Джоггеры">Джоггеры</option>
-                            <option value="Шорты">Шорты</option>
+                            <option value="Шорты">Шорты</option> */}
                         </select>
                         <input
                             className="input"
