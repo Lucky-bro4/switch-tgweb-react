78e72266c0e14e2a790d90edace3a6f040029492
Lucky-bro4 <shuiskov2017@gmail.com>
2025-01-14 10:01:00 +0300

    ok

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -11,41 +11,41 @@ export const AppProvider = ({ children }) => {
 
     useEffect(() => {
 
-        // try {
-        //     // const response = fetch(`https://bottry-lucky-bro4.amvera.io/products?chatId=${user.id}`);
-        //     const response = fetch(`https://bottry-lucky-bro4.amvera.io/products`);
-        //     const data = response.json();
+        try {
+            // const response = fetch(`https://bottry-lucky-bro4.amvera.io/products?chatId=${user.id}`);
+            const response = fetch(`https://bottry-lucky-bro4.amvera.io/products`);
+            const data = response.json();
             
-        //     if (data.customer.favorite_items) {
-        //         setFavoriteItems(data.customer.favorite_items)
-        //     }
-        //     if (data.customer.cart_items) {
-        //         setAddedItems(data.customer.cart_items)
-        //     }
-        // } catch (e) {
-        //     console.log('Ошибка при получении списка товаров:', e)
-        // }
-
-        const getProducts = async () => {
-
-            try {
-                const response = await fetch(`https://bottry-lucky-bro4.amvera.io/products?chatId=${user.id}`);
-                // const response = await fetch(`https://bottry-lucky-bro4.amvera.io/products`);
-                const data = await response.json();
-                
-                if (data.customer.favorite_items) {
-                    setFavoriteItems(data.customer.favorite_items)
-                }
-                if (data.customer.cart_items) {
-                    setAddedItems(data.customer.cart_items)
-                }
-
-            } catch (e) {
-                console.log('Ошибка при получении списка товаров в AppContext:', e)
+            if (data.customer.favorite_items) {
+                setFavoriteItems(data.customer.favorite_items)
             }
+            if (data.customer.cart_items) {
+                setAddedItems(data.customer.cart_items)
+            }
+        } catch (e) {
+            console.log('Ошибка при получении списка товаров:', e)
         }
 
-        getProducts();
+    //     const getProducts = async () => {
+
+    //         try {
+    //             const response = await fetch(`https://bottry-lucky-bro4.amvera.io/products?chatId=${user.id}`);
+    //             // const response = await fetch(`https://bottry-lucky-bro4.amvera.io/products`);
+    //             const data = await response.json();
+                
+    //             if (data.customer.favorite_items) {
+    //                 setFavoriteItems(data.customer.favorite_items)
+    //             }
+    //             if (data.customer.cart_items) {
+    //                 setAddedItems(data.customer.cart_items)
+    //             }
+
+    //         } catch (e) {
+    //             console.log('Ошибка при получении списка товаров в AppContext:', e)
+    //         }
+    //     }
+
+    //     getProducts();
 
     }, [user])
 
