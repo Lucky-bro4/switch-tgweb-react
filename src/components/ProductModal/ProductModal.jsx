ed33144209b58261a6ab2964da22e9c1688cb2cf
Lucky-bro4 <shuiskov2017@gmail.com>
2025-01-17 01:31:00 +0300

    ok

--- src/components/ProductModal/ProductModal.jsx
+++ src/components/ProductModal/ProductModal.jsx
@@ -88,7 +88,7 @@ const ProductModal = ({ product, onClose, location }) => {
             tg.MainButton.offClick(handleButtonClick);
         };
 
-    }, [tg]);
+    }, [tg, addedItems]);
 
     // useEffect(() => {
     
