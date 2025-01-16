c6a2552bebe0395ee60bf8705fe6ca1d2261922c
Lucky-bro4 <shuiskov2017@gmail.com>
2025-01-16 15:15:20 +0300

    ok

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -80,107 +80,109 @@ const Favorites = () => {
     }
 
     return (
-        <div className="favorites-section">
-            <h1>
-                <span className="catalog-icon" onClick = {handleHomeClick}>
-                    <img 
-                        src="/Images/mainLogo_withoutRental&Back.png" 
-                        width={40} 
-                        alt="Go to catalog" 
-                        title="Go to Catalog"
-                    />
-                </span>
-                Избранное
-            </h1>
-            <div className="favorites-list">
-                {favoriteProducts.length > 0 ? (
-                    favoriteProducts.map((item) => (
-                        <div 
-                            key={item.id} 
-                            className="favorites-item"
-                            onClick={() => onProductClick(item)}
-                        >
-                            <div className="favorites-image-wrapper">
-                                <img 
-                                    src={item.image[0]} 
-                                    alt={item.category + ' ' + item.brand} 
-                                    className="favorites-image"
-                                />
-                                <div 
-                                    className="favorite-icon-favorites" 
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                        handleFavoriteClick(item);
-                                    }}
-                                >
+        <div>
+            <div className="favorites-section">
+                <h1>
+                    <span className="catalog-icon" onClick = {handleHomeClick}>
+                        <img 
+                            src="/Images/mainLogo_withoutRental&Back.png" 
+                            width={40} 
+                            alt="Go to catalog" 
+                            title="Go to Catalog"
+                        />
+                    </span>
+                    Избранное
+                </h1>
+                <div className="favorites-list">
+                    {favoriteProducts.length > 0 ? (
+                        favoriteProducts.map((item) => (
+                            <div 
+                                key={item.id} 
+                                className="favorites-item"
+                                onClick={() => onProductClick(item)}
+                            >
+                                <div className="favorites-image-wrapper">
                                     <img 
-                                        src={
-                                            favoriteItems.some(product => product.id === item.id)
-                                                ? "/Images/icons/icon-already-add.png"
-                                                : "/Images/icons/icon-not-add.png"
-                                        } 
-                                        alt={
-                                            favoriteItems.some(product => product.id === item.id)
-                                                ? "Remove from Favorites"
-                                                : "Add to Favorites"
-                                        }
-                                        className={
-                                            favoriteItems.some(product => product.id === item.id) ? "active" : ""
-                                        }
+                                        src={item.image[0]} 
+                                        alt={item.category + ' ' + item.brand} 
+                                        className="favorites-image"
                                     />
+                                    <div 
+                                        className="favorite-icon-favorites" 
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            handleFavoriteClick(item);
+                                        }}
+                                    >
+                                        <img 
+                                            src={
+                                                favoriteItems.some(product => product.id === item.id)
+                                                    ? "/Images/icons/icon-already-add.png"
+                                                    : "/Images/icons/icon-not-add.png"
+                                            } 
+                                            alt={
+                                                favoriteItems.some(product => product.id === item.id)
+                                                    ? "Remove from Favorites"
+                                                    : "Add to Favorites"
+                                            }
+                                            className={
+                                                favoriteItems.some(product => product.id === item.id) ? "active" : ""
+                                            }
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            
-                            <div>
-                                <div className="favorites-details">
-                                    <h2>{item.category + ' ' + item.brand}</h2>
-                                    <p className="favorites-price">Цена: {item.price} ₽</p>
-                                    <p>Размер бренда: {item.brandSize}</p>
-                                    <p>Состояние: {item.condition}</p>
-                                </div>
-                                <div className="cart-button-container">
-                                    {addedItems.some(product => product.id === item.id) ? (
-                                        <div>
+                                
+                                <div>
+                                    <div className="favorites-details">
+                                        <h2>{item.category + ' ' + item.brand}</h2>
+                                        <p className="favorites-price">Цена: {item.price} ₽</p>
+                                        <p>Размер бренда: {item.brandSize}</p>
+                                        <p>Состояние: {item.condition}</p>
+                                    </div>
+                                    <div className="cart-button-container">
+                                        {addedItems.some(product => product.id === item.id) ? (
+                                            <div>
+                                                <button 
+                                                    className="already-in-cart" 
+                                                >
+                                                    Товар в корзине
+                                                </button>
+                                            </div>
+                                        ) : (
                                             <button 
-                                                className="already-in-cart" 
+                                                className="add-to-cart"
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    handleCartClick(item);
+                                                }}
                                             >
-                                                Товар в корзине
+                                                Добавить в корзину
                                             </button>
-                                        </div>
-                                    ) : (
-                                        <button 
-                                            className="add-to-cart"
-                                            onClick={(e) => {
-                                                e.stopPropagation();
-                                                handleCartClick(item);
-                                            }}
-                                        >
-                                            Добавить в корзину
-                                        </button>
-                                    )}
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
-                ) : (
-                    <div>В избранном ничего нет</div>
-                )}
-                {isModalOpen && selectedProduct && (
-                    <ProductModal
-                        product={selectedProduct} 
-                        onClose={closeModal}
-                        // addedItems={addedItems}
-                        // setAddedItems={setAddedItems}
-                        // favoriteItems={favoriteItems}
-                        // setFavoriteItems={setFavoriteItems}
-                    />
+                        ))
+                    ) : (
+                        <div>В избранном ничего нет</div>
+                    )}
+                    {isModalOpen && selectedProduct && (
+                        <ProductModal
+                            product={selectedProduct} 
+                            onClose={closeModal}
+                            // addedItems={addedItems}
+                            // setAddedItems={setAddedItems}
+                            // favoriteItems={favoriteItems}
+                            // setFavoriteItems={setFavoriteItems}
+                        />
+                    )}
+                </div>
+                {favoriteItems.length > 0 && (
+                    <div className="total-price">
+                        <h2>Общая сумма: {totalPrice} ₽</h2>
+                    </div>
                 )}
             </div>
-            {favoriteItems.length > 0 && (
-                <div className="total-price">
-                    <h2>Общая сумма: {totalPrice} ₽</h2>
-                </div>
-            )}
             <Footer />
         </div>
     );
