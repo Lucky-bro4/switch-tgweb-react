abd8e0daf62f2c25ac97c9e3fb8fbe9e0f267cd2
Lucky-bro4 <shuiskov2017@gmail.com>
2025-01-19 20:20:56 +0300

    ok

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -24,7 +24,9 @@ export const AppProvider = ({ children }) => {
                 const data = await response.json();
 
                 if (data.error) {
+                    tg.showAlert('Перезапустите бота командой /start в чате');
                     tg.close();
+                    return
                 }
 
                 if (data.products) {
