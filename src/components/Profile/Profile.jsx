e42b06030fca4f62e4ab51954f9645d4e51d5d78
Lucky-bro4 <shuiskov2017@gmail.com>
2025-01-22 14:36:36 +0300

    ok

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTelegram } from '../../hooks/useTelegram';
 import { AppContext } from '../../context/AppContext';
